422da29bf33c6b0678b2541ad00f6d2d95c69ad8
Ye.S <small_black_luo@163.com>
2022-10-01 20:39:20 +0800

    2022-10-01-增加doc

---
+++ utils/crawler.py
@@ -0,0 +1,15 @@
+import requests
+
+def crawlTimesCalculation(url:str)->int:
+    
+
+
+def zuelCrawler(url:str,savepath:str,crawltimes:int):
+    """
+    :param url: 待爬取页面的链接
+    :param savepath: 存储爬取结果的路径
+    :param crawltimes: 需要爬取的次数
+    :return:  
+    """
+
+    return None
