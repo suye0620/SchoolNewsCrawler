6744bdc6a7e4055cf91c0a0769df7fb303f6a576
Ye.S <small_black_luo@163.com>
2022-10-05 00:37:39 +0800

    2022-10-05-不写了md

--- utils/crawler.py
+++ utils/crawler.py
@@ -13,7 +13,7 @@ class zuelCrawler:
         self.list_to_visit = []
         self.page_num = 0
 
-    def countPageNum(self):
+    def countPageNum(self)->int:
         """
         :update page_num: 该站点需要翻页的次数,用于生成待爬取url
         """
@@ -24,7 +24,8 @@ class zuelCrawler:
             # 使用bs解析网页，获取要翻页的次数
             bs_site = BeautifulSoup(response.text,"html.parser")
             self.page_num = bs_site.find(name='em',attrs={'class':'all_pages'}).get_text()
-            print(self.page_num)
+            # print(self.page_num)
+            return int(self.page_num)
         else:
             print("访问站点遇到了问题")
     
@@ -34,32 +35,37 @@ class zuelCrawler:
         :param endpage:要获取链接的最后一页
         :update list_to_visit: 获取所有页上新闻的url链接,存入list_to_visit
         """
-        temp = "http://wellan.zuel.edu.cn/1668/list{}.htm".format(1)
-        try:
-            response = requests.get(temp,headers=self.headers).content.decode()
-            bs_onepage =  BeautifulSoup(response,"lxml")
-            # list_urls = bs_onepage.find_all("span",attrs={'class':'Article_Title'})
-            list_urls = bs_onepage.select('ul > li.list_item > div.fields.pr_fields > span.Article_Title > a')
-            df = pd.DataFrame(columns=['link','title','site_name','pagenum',])
-            for url in list_urls:
-                list_info = []
-                list_info.append('http://wellan.zuel.edu.cn'+url.attrs['href'])
-                list_info.append(url.attrs['title'])
-                list_info.append(self.site_name)
-                list_info.append(1)
-                df.loc[df.shape[0]] = list_info
-            df.to_csv("./data/urls.csv",encoding='utf-8-sig',index = False)
-        except requests.exceptions.RequestException:
-            return None
-        # for i in range(startpage,endpage+1):
-        #     temp = "http://wellan.zuel.edu.cn/1668/list{}.htm".format(i+1)
-        #     try:
-        #         response = requests.get(temp,headers=self.headers)
-        #         bs_onepage =  BeautifulSoup(response,"html.parser")
+        for i in range(startpage,endpage+1):
+            temp = "http://wellan.zuel.edu.cn/1668/list{}.htm".format(i)
+            try:
+                response = requests.get(temp,headers=self.headers).content.decode()
+                bs_onepage =  BeautifulSoup(response,"lxml")
+                # list_urls = bs_onepage.find_all("span",attrs={'class':'Article_Title'})
+                list_urls = bs_onepage.select('ul > li.list_item > div.fields.pr_fields > span.Article_Title > a')
+                df = pd.DataFrame(columns=['link','title','site_name','pagenum',])
+                for url in list_urls:
+                    list_info = []
+                    list_info.append('http://wellan.zuel.edu.cn'+url.attrs['href'])
+                    list_info.append(url.attrs['title'])
+                    list_info.append(self.site_name)
+                    list_info.append(i)
+                    # append方法要被废弃了，有点无语，暂时用下面的写法
+                    df.loc[df.shape[0]] = list_info
+                df.to_csv("./data/urls.csv",encoding='utf-8-sig',index = False,mode='a')
+            except requests.exceptions.RequestException:
+                return None
 
-        #     except requests.exceptions.RequestException:
-        #         return None
+    def parse(self,url):
+        """
+        :param url: 需要解析的一个页面
+        :return article_content: 文章内容
+        """
+        try:
+            res = requests.get(url,headers=self.headers).content.decode()
+            bs_onearticle = BeautifulSoup(res,'lxml')
             
+        except requests.exceptions.RequestException as e:
+            print(e)
 
 
     
