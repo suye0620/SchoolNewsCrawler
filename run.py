e3a6da1a6a2dbaaaa2876a068c8885da91664fd6
Ye.S <small_black_luo@163.com>
2022-10-05 20:52:10 +0800

    2022-10-05-1-zuel爬虫开发完成

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from utils.crawler import zuelCrawler
+from utils.crawler import ZuelCrawler
 
 if __name__ == "__main__":
     # 伪装的请求头
@@ -7,6 +7,12 @@ if __name__ == "__main__":
     }
 
     # 文澜要闻
-    zuelCrawler_1668 = zuelCrawler("http://wellan.zuel.edu.cn/1668/list.htm","文澜要闻",headers = one_header)
-    # zuelCrawler_1668.countPageNum()
-    zuelCrawler_1668.getUrls(1,2)
+    zuelCrawler_1668 = ZuelCrawler("http://wellan.zuel.edu.cn/1668/list.htm","文澜要闻",headers = one_header)
+    # 数要翻页的次数
+    zuelCrawler_1668.countPageNum()
+    # 获取要爬取的url
+    zuelCrawler_1668.getUrls(startpage = 1,endpage = zuelCrawler_1668.getPageNum(),urlsfile_path="./data/zuelurls.csv")
+    # 测试parse函数
+    # zuelCrawler_1668.parse(url='http://wellan.zuel.edu.cn/2022/0921/c1668a306614/page.htm')
+    # 爬取
+    zuelCrawler_1668.crawl(urlsfile_path="./data/urls.csv",save_path="./data/zuel_news/")
--- utils/crawler.py
+++ utils/crawler.py
@@ -1,8 +1,10 @@
-import requests
+from time import sleep
 from bs4 import BeautifulSoup
+import requests
 import pandas as pd
+from tqdm import tqdm
 
-class zuelCrawler:
+class ZuelCrawler:
     """
     爬取文澜新闻网的爬虫
     """
@@ -13,7 +15,7 @@ class zuelCrawler:
         self.list_to_visit = []
         self.page_num = 0
 
-    def countPageNum(self)->int:
+    def countPageNum(self):
         """
         :update page_num: 该站点需要翻页的次数,用于生成待爬取url
         """
@@ -24,18 +26,19 @@ class zuelCrawler:
             # 使用bs解析网页，获取要翻页的次数
             bs_site = BeautifulSoup(response.text,"html.parser")
             self.page_num = bs_site.find(name='em',attrs={'class':'all_pages'}).get_text()
-            # print(self.page_num)
-            return int(self.page_num)
         else:
             print("访问站点遇到了问题")
     
-    def getUrls(self,startpage:int,endpage:int):
+    def getPageNum(self)->int:
+        return self.page_num
+    
+    def getUrls(self,startpage:int,endpage:int,urlsfile_path:str):
         """
         :param startpage:要获取链接的第一页
         :param endpage:要获取链接的最后一页
         :update list_to_visit: 获取所有页上新闻的url链接,存入list_to_visit
         """
-        for i in range(startpage,endpage+1):
+        for i in tqdm(range(startpage,endpage+1),desc='getUrls:'):
             temp = "http://wellan.zuel.edu.cn/1668/list{}.htm".format(i)
             try:
                 response = requests.get(temp,headers=self.headers).content.decode()
@@ -51,21 +54,41 @@ class zuelCrawler:
                     list_info.append(i)
                     # append方法要被废弃了，有点无语，暂时用下面的写法
                     df.loc[df.shape[0]] = list_info
-                df.to_csv("./data/urls.csv",encoding='utf-8-sig',index = False,mode='a')
+                df.to_csv(urlsfile_path,encoding='utf-8-sig',index = False,mode='a')
             except requests.exceptions.RequestException:
                 return None
 
-    def parse(self,url):
+    def parse(self,url)->list:
         """
         :param url: 需要解析的一个页面
-        :return article_content: 文章内容
+        :return article_content: 包含文章内容的所有tag的list
         """
         try:
             res = requests.get(url,headers=self.headers).content.decode()
             bs_onearticle = BeautifulSoup(res,'lxml')
-            
+            article_content = bs_onearticle.select("div.wp_articlecontent > p")
+            # article_content是一个列表，索引0取第一个，然后调用get_text方法
+            return [p.get_text()+'\n' for p in article_content if p.get_text() != '']
         except requests.exceptions.RequestException as e:
             print(e)
+
+    def crawl(self,urlsfile_path:str,save_path:str):
+        """
+        :param urlsfile_path: 需要爬取的网址文件
+        """
+        df = pd.read_csv(urlsfile_path,encoding='utf-8-sig')
+        nrow = df.shape[0]
+        for row in tqdm(range(nrow),desc='crawling...'):
+            article_content = self.parse(df['link'][row])
+            # 生成存储文件名
+            txtfile_name = df['link'][row][26:-9].replace("/","_")+".txt"
+            with open(file=(save_path+txtfile_name),mode='w',encoding='utf-8') as f:
+                f.writelines(article_content)
+            # 完成写入，关闭文件
+            f.close()
+            if row%11 == 0:
+                sleep(5)
+        
 
 
     
